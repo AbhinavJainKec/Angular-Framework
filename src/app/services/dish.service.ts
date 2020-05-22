91fb62b58140b6b1e95ed5a2e287614a6ee281c5
Abhinav Jain <abhinav.jain@krishnacollege.ac.in>
2020-05-22 17:07:08 +0530

    Promise 2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
-import { from } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +10,23 @@ export class DishService {
   constructor() { }
 
   getDishes(): Promise<Dish[]> {
-    return Promise.resolve(DISHES);
+    return new Promise(resolve => {
+      // Stimulate server latency with 2 second delay
+      setTimeout(() => resolve(DISHES), 2000);
+    });
   }
 
   getDish(id: string): Promise<Dish> {
-    return Promise.resolve(DISHES.filter((dish) => (dish.id === id))[0]);
+    return new Promise(resolve => {
+      // Stimulate server latency with 2 second delay
+      setTimeout(() => resolve(DISHES.filter((dish) => (dish.id === id))[0]), 2000);
+    });
   }
 
   getFeaturedDish(): Promise<Dish> {
-    return Promise.resolve(DISHES.filter((dish) => dish.featured)[0]);
+    return new Promise(resolve => {
+      // Stimulate server latency with 2 second delay
+      setTimeout(() => resolve(DISHES.filter((dish) => dish.featured)[0]), 2000);
+    });
   }
 }
